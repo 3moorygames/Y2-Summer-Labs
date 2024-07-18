653fb9372a9418e059db7276a7d037e2df1c5d11
3moorygames <amooory9998@gmail.com>
2024-07-18 11:51:47 +0300

    Omar Shaheen :)

--- Flask Form Fortune Teller/fortune.py
+++ Flask Form Fortune Teller/fortune.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for,redirect,request
 import random
 
 app = Flask(__name__,template_folder="templates", static_folder="static" )
-
+    
 fortunes = [
     "You will soon embark on a journey that will lead to unexpected opportunities.",
     "A surprise gift will come your way, bringing joy and delight.",
@@ -17,15 +17,29 @@ fortunes = [
 ]
 
 
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-	return render_template('fortune.html')
+    if request.method == 'GET':
+        return render_template('fortune.html')
+    else:
+        birthday = request.form['birthday1']
+        birthday_month_length = len(birthday) - 1
+        return redirect(url_for("fortune", birthday_month_length = birthday_month_length,
+            b = birthday))
 
 
-@app.route('/fortune')
-def fortune():
-	fort = fortunes[random.randint(0,10)] 
-	return render_template('realfortune.html', fortt = fort)
+
+@app.route('/fortune/<int:birthday_month_length>')
+def fortune(birthday_month_length):
+    if birthday_month_length < 10:
+
+        # birth_month_length = b.len()
+        fort = fortunes[birthday_month_length]
+        #random.randint(0,10)] 
+        return render_template('realfortune.html', fortt = fort)
+    else:
+        fort = "You have a bad fortune "
+        return render_template('realfortune.html', fortt = fort)
 
 
 if __name__ == '__main__':
